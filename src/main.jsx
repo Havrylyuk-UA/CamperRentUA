3b954d5b37926a47c02903a23d155901f43f23b2
Havrylyuk Pavlo <havrylyukpavlo95@gmail.com>
2024-06-01 17:32:46 +0300

    fix path

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/CamperRentUA">
       {/* <PersistGate loading={null} persistor={persistor}> */}
       <App />
       {/* </PersistGate> */}
