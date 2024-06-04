016c3744e2b5fece4219a5b2b66bcb3edae30a96
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-06-04 11:46:47 +0000

    Deploying to gh-pages from @ Havrylyuk-UA/CamperRentUA@31da6bb9aef2d1f18176a4fb27b265f9e2c07317 🚀

--- assets/selectors-BcY0Zj5B.js
+++
@@ -1,1 +0,0 @@
-const a="/CamperRentUA/assets/sprite-WJ0KrafU.svg",e=s=>s.cards.cards,t=s=>s.cards.isLoading,c=s=>s.cards.details;export{e as a,c as b,t as c,a as s};
---
+++ assets/selectors-DWHN0dYv.js
@@ -0,0 +1,1 @@
+const a="/CamperRentUA/assets/sprite-WJ0KrafU.svg",e=s=>s.cards.cards,t=s=>s.cards.isLoading,c=s=>s.cards.details,r=s=>s.cards.favorite;export{r as a,a as b,c,t as d,e as s};
